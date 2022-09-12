d217c3c5ab07e77070d4838d4317fd6a4b48ad95
Ivanna Pokhyl <ivanna.pokhyl@gmail.com>
2022-09-12 18:41:23 +0300

    Innitial commit

---
+++ script/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    function navbarShrink() {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+})
